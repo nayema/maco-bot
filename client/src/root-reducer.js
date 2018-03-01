5f8df066453ce242e812c002568c97673e32cbff
Nayema Nasir <nayema.nasir89@gmail.com>
2018-02-28 23:27:13 -0500

    Connect reducer to root-reducer

--- client/src/root-reducer.js
+++ client/src/root-reducer.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 
+import * as clientsEditor from './clients-editor'
+
 export default combineReducers({
-  routing: routerReducer
+  routing: routerReducer,
+  clientsEditor: clientsEditor.reducer
 })
