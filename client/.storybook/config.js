933d7e6d908544699c92086889ecdc941e75e0d7
Nayema Nasir <nayema.nasir89@gmail.com>
2018-04-02 18:16:05 -0400

    Fix snapshots with new edits

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -4,6 +4,7 @@ function loadStories () {
   require('../src/routes/client-details/ClientEditor.stories')
   require('../src/routes/client-list/ClientsList.stories')
   require('../src/routes/product-details/ProductEditor.stories')
+  // require('../src/routes/equipment-list/EquipmentList.stories')
 }
 
 configure(loadStories, module)
