53bfaaacd71ef66011d527a8c300fb89c88d3cb0
Nayema Nasir <nayema.nasir89@gmail.com>
2018-04-01 23:51:51 -0400

    Merge Product Details jsx with its container

--- client/src/routes/product-details/ProductDetailsContainer.js
+++ client/src/routes/product-details/ProductDetailsContainer.js
@@ -1,6 +1,18 @@
+import React from 'react'
+import { LinearProgress } from 'material-ui/Progress'
 import { connect } from 'react-redux'
 
-import ProductDetails from './ProductDetails'
+import ProductHeader from './ProductHeaderContainer'
+
+const LoadingProductProgress = () => <LinearProgress mode="query"/>
+
+const ProductDetails = ({ product, loadingProduct }) =>
+  <div>
+    {loadingProduct && <LoadingProductProgress loadingProduct={loadingProduct}/>}
+    {product && <div>
+      <ProductHeader/>
+    </div>}
+  </div>
 
 const mapStateToProps = (state) => ({
   product: state.pages.productDetails.product,
